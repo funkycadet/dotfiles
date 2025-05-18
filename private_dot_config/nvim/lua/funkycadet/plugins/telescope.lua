b8dcd3ee056f962073edc8ff0d911a642bbc967e
Eric Alaribe <ericsmithsonian2@gmail.com>
2025-05-18 19:44:49 +0100

    update: add extensions for telescope and custom default settings

--- private_dot_config/nvim/lua/funkycadet/plugins/telescope.lua
+++ private_dot_config/nvim/lua/funkycadet/plugins/telescope.lua
@@ -1,15 +1,28 @@
-return {
-	'nvim-telescope/telescope.nvim',
-	branch = '0.1.8',
-	'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'},
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		'sharkdp/fd',
-	},
-	config = function ()
-		local telescope = require('telescope')
-		vim.keymap.set('n', '<Leader>f', telescope, {})
-		vim.keymap.set('n', '<Leader>ff', telescope.find_files, {})
-		vim.keymap.set('n', '<Leader>s', telescope.session-lens, {})
-	end
+local telescope = require('telescope')
+telescope.setup {
+  defaults = {
+    file_ignore_patterns = { "node_modules", "^.git/", "dist" },
+    hidden = true,
+    follow = true,
+  },
+  extensions = {
+    file_browser = {
+      hijack_netrw = true,
+    }
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+    live_grep = {
+      additional_args = function()
+        return { "--hidden", "--no-ignore"}
+      end
+    }
+  }
 }
+telescope.load_extension('fzf')
+telescope.load_extension('dap')
+telescope.load_extension('file_browser')
+-- Run the `configurations` picker from nvim-dap
+-- telescope.extensions.dap.configurations()
